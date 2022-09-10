8bbabedcde58219e3699fbf66debccf4e194def1
Davi Suga <daviciencia1@gmail.com>
2022-09-10 13:28:52 -0300

    generated js removal

--- examples/expo/todos/App.js
+++ examples/expo/todos/App.js
@@ -3,6 +3,7 @@ import { StatusBar } from "expo-status-bar";
 import * as NativeBase from "native-base";
 import { NativeBaseProvider } from "native-base";
 import React from "react";
+import { Feather, Entypo } from "@expo/vector-icons";
 import { make as Component } from "./src/Component.bs";
 export default function App() {
   return (
@@ -16,11 +17,14 @@ export default function App() {
         alignItems="center"
         background="primary.700"
       >
-        <NativeBase.Text size="" fontWeight="bold" color="black">
+        <NativeBase.Text mt="10" fontWeight="bold" color="black">
           Hey
         </NativeBase.Text>
-        <NativeBase.IconButton colorScheme={""} size={""} onPress={(e) => {}} />
+        <NativeBase.IconButton onPress={(e) => {}} />
       </NativeBase.Box>
+      {/* <NativeBase.Icon as={Entypo} name="minus" size="xs" color="trueGray.400" /> */}
+      {/* <NativeBase.Checkbox  size=""  /> */}
+
       {/* <NativeBase.Input variant={["filled", "rounded"]} ></NativeBase.Input> */}
     </NativeBaseProvider>
   );
