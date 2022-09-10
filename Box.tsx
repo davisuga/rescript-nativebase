d7525a46cccfe4962316fa95746c93f400a86b63
Davi William Moraes Suga <daviciencia1@gmail.com>
2022-09-10 18:17:40 -0300

    removed unused code

--- Box.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { Box } from "native-base";
-import { InterfaceBoxProps } from "native-base/lib/typescript/components/primitives/Box";
-let a = <Box margin=""></Box>;
-type x = InterfaceBoxProps;
